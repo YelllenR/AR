74a4d62a8ea417c667a3ddc4862883b15f357d12
YelllenR <aurelie@rosedarbon.com>
2023-02-16 10:04:30 +0100

    testing configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
-// import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess';
 const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
 
 	preprocess: preprocess(),
 	kit: {
 
+		target: '#svelte',
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			precompress: false,
-			strict: true
-		}),
-
-		paths: {
-			base: dev ? '' : '/AR',
-		},
-
-		appDir: 'project'
+			pages: 'build',  // path to public directory
+			assets: 'build',  // path to public directory
+			fallback: null
+		})
 	}
-
 };
-
-
 export default config;
