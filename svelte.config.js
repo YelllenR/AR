6cc4547423cf596adf9590f3e5cff8a04173a890
YelllenR <aurelie@rosedarbon.com>
2023-02-16 10:32:08 +0100

    json modifications

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 
 const config = {
-
 	preprocess: preprocess({
 		scss: true
 	}),
 	kit: {
-
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
